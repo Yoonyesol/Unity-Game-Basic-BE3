ffecc548315c59ffa0fecd47ba970f65a416d4fc
yoon yesol <maniac970430@naver.com>
2021-01-01 23:01:33 +0900

    make a map

---
+++ Unity-Game-Basic-BE3-/Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float Speed;
+    float h;
+    float v;
+    bool isHorizonMove;
+
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Move Value
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        //Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonDown("Horizontal");
+        bool vUp = Input.GetButtonDown("Vertical");
+        
+        //Check Horizontal Move
+        if (hDown)
+            isHorizonMove = true; //수평이동
+        else if (vDown)
+            isHorizonMove = false; //수직이동
+    }
+
+    private void FixedUpdate()
+    {
+        //Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed; 
+    }
+}
